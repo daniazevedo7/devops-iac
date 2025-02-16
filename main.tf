68ea033958306c2cc2e14bab7dfd56f136d5b913
Daniel Azevedo <d.azevedo.s7@gmail.com>
2025-02-16 11:31:30 -0300

    comentado o user_data

--- main.tf
+++ main.tf
@@ -17,12 +17,12 @@ resource "aws_instance" "app_server" {
   ami           = "ami-04b4f1a9cf54c11d0"
   instance_type = "t2.micro"
   key_name = "iac-alura"
-  user_data = <<-EOF
+ /*  user_data = <<-EOF
   #!/bin/bash
   cd /home/ubuntu
   echo "<h1> Feito com o Terraform</h1>" > index.html
   nohup busybox httpd -f -p 8008 &
-  EOF    
+  EOF     */
   tags = {
     Name = "teste-aws"
   }
